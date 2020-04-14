875209b3f7db24cab474fbd566e4520cd18a590e
babaoriley <helenebrulin@live.fr>
2020-04-14 17:28:27 +0200

    init

--- philo_one/init.c
+++ philo_one/init.c
@@ -6,13 +6,45 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/14 17:03:17 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/04/14 17:06:00 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/04/14 17:28:16 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static void init_philos(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->number_of_philosophers)
+	{
+		data->philos[i].id = i;
+		data->philos[i].eat_count = 0;
+		data->philos[i].state = NOTHING;
+		data->philos[i].rfork_available = 1;
+		data->philos[i].lfork_available = 1;
+		i++;
+	}
+}
+
+
 int		init_data(t_data *data, int argc, char **argv)
 {
-
+	data->number_of_philosophers = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->must_eat_nb = ft_atoi(argv[5]);
+	else
+		data->must_eat_nb = 0;
+	if (data->number_of_philosophers < 2 || data->must_eat_nb < 0) //autres cas?
+		return (1);
+	data->start = get_timestamp();
+	if (!(data->philos =
+		(t_philo*)malloc(sizeof(*(data->philos)) * data->number_of_philosophers)))
+		return (1);
+	init_philos(data);
+	return (0);
 }
--- philo_one/philo.h
+++ philo_one/philo.h
@@ -6,7 +6,7 @@
 /*   By: hbrulin <hbrulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/14 16:24:20 by hbrulin           #+#    #+#             */
-/*   Updated: 2020/04/14 17:13:49 by hbrulin          ###   ########.fr       */
+/*   Updated: 2020/04/14 17:27:48 by hbrulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@
 #include <pthread.h>
 #include <unistd.h>
 
-# define EAT 	0
-# define SLEEP  1
-# define THINK	2
-# define DIED 	3
+# define NOTHING 0
+# define EAT 	1
+# define SLEEP  2
+# define THINK	3
+# define DIED 	4
 
 typedef struct		s_philo
 {
@@ -37,7 +38,7 @@ typedef struct s_data
 	unsigned long time_to_die;
 	unsigned long time_to_eat;
 	unsigned long time_to_sleep;
-	int number_of_time_each_philosophers_must_eat;
+	int must_eat_nb;
 	unsigned long start;
 	t_philo			*philos;
 }				t_data;
@@ -47,6 +48,7 @@ int				ft_error(char *s);
 int		init_data(t_data *data, int argc, char **argv);
 void	ft_putstr(char *s);
 void	ft_putnbr(unsigned long n);
+int	ft_atoi(const char *str);
 
 
 #endif
